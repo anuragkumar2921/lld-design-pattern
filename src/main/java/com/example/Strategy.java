aa82a3528fe44b43d5316f0aa52cd7c5f3619d7b
anuragkumar <anuragkumar5144@gmail.com>
2025-09-11 02:19:19 +0530

    first commit

---
+++ src/main/java/com/example/Strategy.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Strategy Pattern
+ *
+ */
+public class Strategy {
+  public static void main(String[] args) {
+    System.out.println("This is strategy pattern");
+  }
+}
